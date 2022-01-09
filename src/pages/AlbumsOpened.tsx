ad8b69c2e090f7922b306384f33afa3ba6dc6027
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-01-08 22:34:23 -0300

    created back button on AlbumsOpened

--- src/pages/AlbumsOpened.tsx
+++ src/pages/AlbumsOpened.tsx
@@ -1,10 +1,11 @@
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { api } from '../api';
 import { AlbumOpened } from '../types/AlbumOpened';
 
 export const AlbumsOpened = () => {
     const params = useParams();
+    const navigate = useNavigate();
 
     const [album, setAlbum] = useState<AlbumOpened[]>([]);
 
@@ -16,16 +17,20 @@ export const AlbumsOpened = () => {
         let json = await api.getAlbum();
         setAlbum(json);
     }
+
+    const backButton = () => {
+        navigate(-1);
+    }
     return (
         <div>
             <h1>Galeria de Fotos: {album.length}</h1>
             <hr />
-            <button>Voltar</button>
+            <button onClick={backButton}>Voltar</button>
             <div>
                 {album.map((item, index) => (
                     <div key={index}>
                         <h4>{item.title}</h4>
-                        <li><img src={item.thumbnailUrl} /></li>
+                        <img src={item.thumbnailUrl} />
                     </div>
                 ))}
             </div>
